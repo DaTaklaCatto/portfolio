525bcef4b62adef5b74126448f8c0efb5360d48f
Nadid Wasique <76746289+DaTaklaCatto@users.noreply.github.com>
2026-09-18 00:13:51 +0600

    PWA bug fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,14 @@
 (function () {
   "use strict";
 
+  // Stop PWA
+  function stopPWA() {
+    window.addEventListener("beforeistallprompt", (e) => {
+      e.preventDefault();
+    });
+  }
+  stopPWA();
+
   // Register GSAP Plugins
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, CustomEase);
 
@@ -18,7 +26,7 @@
   } else {
     initLucide();
   }
-
+  3;
   /* ==========================================================================
         Live Dhaka Time Clock
      ========================================================================== */
